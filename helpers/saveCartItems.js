76b0f3f773d6217482ee4d98a113b2f006d71630
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-09-13 19:45:22 -0400

    requisito 6

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,6 +1,4 @@
-const saveCartItems = () => {
-  // seu código aqui
-};
+const saveCartItems = (elemento) => localStorage.setItem('getItem', elemento);
 
 if (typeof module !== 'undefined') {
   module.exports = saveCartItems;
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -1,9 +1,14 @@
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const saveCartItems = require('../helpers/saveCartItems');
 
+
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa execução da função com o parametro cartItem', () => {
+    expect(saveCartItems(localStorage.getItem)).toHaveBeenCalled();
+  })
+  it('Testa dois parametros ', () => {
+    expect(saveCartItems(localStorage.getItem, 'elemento')).toHaveBeenCalled();
+  })
 });
